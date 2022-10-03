294110486d743d8229eed9d47c1c6eb0d9cc7264
Ajay Wani <ajay.wani1986@gmail.com>
2022-10-03 11:15:52 +0530

    Addition program by Ajay

---
+++ src/com/gitpractice/Addition.java
@@ -0,0 +1,9 @@
+package com.gitpractice;
+
+public class Addition {
+    public static void main(String[] args) {
+        int a=5,b=10,c;
+        c=a+b;
+        System.out.println("Addition of a and b is- "+c);
+    }
+}
